c094220b6d60cb298d13da19ad3ea68bf834ea03
ZAHIN <abuzahinmohammadnowsin@gmail.com>
2025-02-14 15:08:57 +0600

    Dashboard to use Soft UI Dashboard template

---
+++ resources/js/Layouts/MasterLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="g-sidenav-show bg-gray-100">
+        <sidebar />
+        <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg">
+            <Navbar/>
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+            <Footer/>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import Sidebar from '@/Components/Sidebar/Sidebar.vue';
+import Navbar from '@/Components/Navbar/Navbar.vue';
+import Footer from '@/Components/Footer/Footer.vue';
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,12 +10,30 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Admin Template Fonts and Icons -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+        <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+        <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+        <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+        <!-- Admin Template CSS -->
+        <link id="pagestyle" href="{{ asset('assets/css/soft-ui-dashboard.css?v=1.0.3') }}" rel="stylesheet" />
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="g-sidenav-show bg-gray-100">
         @inertia
+
+        <!-- Admin Template JS -->
+        <script src="{{ asset('assets/js/core/popper.min.js') }}"></script>
+        <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+        <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
+        <script src="{{ asset('assets/js/soft-ui-dashboard.min.js?v=1.0.3') }}"></script>
+
     </body>
 </html>
